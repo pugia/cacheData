714c925dba4e057c87305a47159018d51c0c98e7
Marco Pugliese <marco.pugliese@pugia.com>
2015-10-20 16:22:34 +0200

    v1.0.0

---
+++ cacheData.js
@@ -0,0 +1,52 @@
+(function ($) {
+
+	var PACache = {};
+	
+	function checksum(s) {
+		var hash = 0,strlen = s.length,i,c;
+		if ( strlen === 0 ) { return hash; }
+		for ( i = 0; i < strlen; i++ ) {
+			c = s.charCodeAt( i );
+			hash = ((hash << 5) - hash) + c;
+			hash = hash & hash; // Convert to 32bit integer
+		}
+		return hash;
+	}
+	
+	$.cacheData = function(url, params, method) {
+		
+		var dfrd = $.Deferred();
+		
+		if (typeof url === 'undefined') { return dfrd.reject(); }
+		if (typeof method !== 'string') { method = 'get'; }
+		if (typeof params !== 'object') { params = {}; }	
+		
+		var key = checksum(JSON.stringify([url,	method,params]));
+		
+		if (PACache[key]) { return PACache[key]; } 
+		else {
+			
+			// init key element to prevent multiple calls on same url
+			PACache[key] = 1;
+			
+			$.ajax({
+			  method: method,
+			  url: url,
+			  data: params
+			})	
+			.done(function(response) {
+				dfrd.resolve(response);
+			})
+			.fail(function() {
+				PACache[key] = false;
+				dfrd.reject();
+			});
+					
+			PACache[key] = dfrd.promise();
+			return PACache[key];
+			
+		}
+			
+	};	
+
+}(jQuery));
